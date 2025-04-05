664ff1733df3d6ff91a4c945eacff0bd7ed049ac
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-05 23:47:57 +0530

    Update tb_decimal_to_other.v

--- number_conversions/decimal_to_other/tb_decimal_to_other.v
+++ number_conversions/decimal_to_other/tb_decimal_to_other.v
@@ -1,12 +1,12 @@
 module tbdec;
 reg [3:0]decimal;
-  wire [3:0]octal;
+wire [3:0]octal;
 wire [3:0]hexa;
 wire [3:0]binary;
 dec uut (binary, octal, hexa, decimal) ;
 initial begin
 repeat (20) begin
-  #5 decimal={$random}%10;
+#5 decimal={$random}%10;
 end
 #10 $finish;
 end
