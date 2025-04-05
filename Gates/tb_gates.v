2852c8a2a02007f6e83745c54c20339dd9505aef
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-05 19:49:09 +0530

    Update tb_gates.v

--- Gates/tb_gates.v
+++ Gates/tb_gates.v
@@ -1,1 +1,13 @@
-
+module tbgates;
+reg a,b, control;
+wire y;
+gates uut (y, a,b, control);
+initial
+begin
+a=0;b=0; control=1;
+#5 a=0;b=1; control=0;
+#5 a=1;b=0; control=0;
+#5 a=1;b=1; control=1;
+#5 sfinish;
+end
+endmodule
