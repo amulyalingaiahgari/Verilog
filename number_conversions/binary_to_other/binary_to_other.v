296e7d0ba0ae02be586e1d1cd1ba1f9e789805a2
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-05 23:44:11 +0530

    Update binary_to_other.v

--- number_conversions/binary_to_other/binary_to_other.v
+++ number_conversions/binary_to_other/binary_to_other.v
@@ -1,4 +1,4 @@
-module binary (octal,dec, hexa, binary);
+module binary (octal, dec, hexa, binary);
 input [7:0]binary;
 output reg [31:0]dec;
 output reg [7:0]hexa;
@@ -8,7 +8,7 @@ always @(binary) begin
 
 //binary to hexa
 hexa[3:0]=binary [3:0];
-hexa[7:4]=binary [7:4] ;
+hexa[7:4]=binary [7:4];
 
 //binary to octal
 octal[2:0]=binary [2:0];
@@ -16,6 +16,6 @@ octal[5:3]=binary [5:3];
 octal[7:6]=binary [7:6];
 
 //binary to decimal
-  dec=(binary[o].2 ** 0)+(binary[1].2 ** 1)+(binary[2].2 ** 2)+(binary[3].2 ** 3)+(binary[4].2 ** 4)+(binary[5].2 ** 5)+(binary[6].2 ** 6)+(binary[7]*2 ** 7);
+  dec=(binary[0].2 ** 0)+(binary[1].2 ** 1)+(binary[2].2 ** 2)+(binary[3].2 ** 3)+(binary[4].2 ** 4)+(binary[5].2 ** 5)+(binary[6].2 ** 6)+(binary[7]*2 ** 7);
 end
 endmodule
