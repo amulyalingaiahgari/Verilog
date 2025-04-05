c84e616552bf4dda4d1c73692a472e2f4e0743d8
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-05 23:40:24 +0530

    Create tb_hexadecimal_to_other.v

---
+++ number_conversions/hexadecimal_to_other/tb_hexadecimal_to_other.v
@@ -0,0 +1,13 @@
+module tbhexa;
+reg [3:0]hexa;
+wire (3:0]octal;
+wire [4:0]decimal;
+wire [3:0]binary;
+hexa uut (binary, octal, decimal, hexa);
+initial begin
+  repeat (20) begin
+  #5 hexa={$random}%16;
+  end
+  #10 $finish;
+end
+endmodule
