fb301d868985e29cada338d5d1adec4bb76e8d49
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-05 23:25:45 +0530

    Create tb_binary_to_other.v

---
+++ number_conversions/binary_to_other/tb_binary_to_other.v
@@ -0,0 +1,15 @@
+module tbbinary;
+reg [7:0]binary;
+wire (7:0]dec;
+wire [7:0]hexa;
+wire [7:0]octal;
+binary dut (octal,dec,hexa, binary);
+initial
+begin
+binary=8'b00101010;
+#10 binary=8'b10100010;
+#10 binary=8'b11011001;
+#10 binary=8'b11110011;
+#10 $finish;
+end
+endmodule
