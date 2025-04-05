de2e2754d082b7301832ead4b8b05a926b5cb756
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-05 23:49:52 +0530

    Update hexadecimal_to_other.v

--- number_conversions/hexadecimal_to_other/hexadecimal_to_other.v
+++ number_conversions/hexadecimal_to_other/hexadecimal_to_other.v
@@ -1,19 +1,19 @@
-module hexa (binary, octal, decimal,hexa);
+module hexa (binary, octal, decimal, hexa);
 input [3:0]hexa;
 output reg [3:0]binary;
 output reg [3:0]octal;
 output reg [4:0]decimal;
-  always @(*) begin
+always @(*) begin
 binary=hexa;
 case (hexa)
 4'h0: octal=4'o0;
 4'h1: octal=4'o1;
 4'h2: octal=4'o2;
-4'h3: octal=4'03;
-4'h4:octal=4'04;
+4'h3: octal=4'o3;
+4'h4:octal=4'o4;
 4'h5: octal=4'o5;
-4'h6:octal=4'06;
-4'h7:octal=4'07;
+4'h6:octal=4'o6;
+4'h7:octal=4'o7;
 4'h8:octal=4'o10;
 4'h9:octal=4'o11;
 4'ha: octal=4'o12;
@@ -41,7 +41,7 @@ case (hexa)
 4'hd: decimal=5'd13;
 4'he:decimal=5'd14;
 4'hf :decimal=5'd15;
-default:decimal=5'do;
+default:decimal=5'd0;
 endcase
 end
 endmodule 
