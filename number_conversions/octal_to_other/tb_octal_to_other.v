a2aac1686dc16aeb7586a6cc9a4cd325b76fb97c
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-05 23:29:30 +0530

    Create tb_octal_to_other.v

---
+++ number_conversions/octal_to_other/tb_octal_to_other.v
@@ -0,0 +1,15 @@
+module tboctal;
+reg [2:0]octal;
+wire [3:0]dec;
+wire [3:0]hexa;
+wire [3:0]binary;
+  octal dut (binary,dec,hexa, octal);
+initial
+begin
+octal=2;
+#10 octal=5;
+#10 octal=7;
+#10 octal=4;
+#10 sfinish;
+end
+endmodule
