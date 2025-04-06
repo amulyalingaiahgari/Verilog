5a621a57cfb30f100256abcb3188b34c1f1c3cb3
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-06 18:25:29 +0530

    Update tb_fulladder.v

--- full_adder/tb_fulladder.v
+++ full_adder/tb_fulladder.v
@@ -1,4 +1,4 @@
-module fa_tb;
+        module fa_tb;
 	// Inputs
 	reg a;
 	reg b;
@@ -20,12 +20,12 @@ module fa_tb;
 		a = 0; b = 0; c = 0;
 		#10 a = 0; b = 0; c = 1;
 		#10 a = 0; b = 1; c = 0;
-    #10 a = 0; b = 1; c = 1;
-    #10 a = 1; b = 0; c = 0;
-    #10 a = 1; b = 0; c = 1;
-    #10 a = 1; b = 1; c = 0;
-    #10 a = 1; b = 1; c = 1;
+		#10 a = 0; b = 1; c = 1;
+		#10 a = 1; b = 0; c = 0;
+		#10 a = 1; b = 0; c = 1;
+		#10 a = 1; b = 1; c = 0;
+		#10 a = 1; b = 1; c = 1;
 		#10 $finish;
 	end
       
-endmodule
+        endmodule
