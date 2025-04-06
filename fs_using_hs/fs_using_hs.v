0c2a3d49f7a02c7c081a4d2960052b1c25404e82
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-06 18:53:03 +0530

    Create fs_using_hs.v

---
+++ fs_using_hs/fs_using_hs.v
@@ -0,0 +1,21 @@
+module fs_using_hs(diff,borrow,a,b,c);
+    input a,b,c;
+    output diff,borrow;
+
+	wire w1,w2,w3;
+
+	hs h1(a,b,w1,w2);
+	hs h2(w1,c,diff,w3);
+	or o1(borrow,w2,w3);
+
+endmodule
+
+
+module hs(diff,borrow,a,b);
+	input a,b;
+	output diff,borrow;
+	
+	assign diff = a^b;
+  assign borrow = (~a)&b;
+	
+endmodule
