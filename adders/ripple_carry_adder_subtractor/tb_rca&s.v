58845e7905fb6f3fc2f3e674bec3a8689892b213
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-07 22:54:28 +0530

    Create tb_rca&s.v

---
+++ adders/ripple_carry_adder_subtractor/tb_rca&s.v
@@ -0,0 +1,21 @@
+module tbas ;
+reg [3:0]A, B;
+reg cin;
+wire [3:0]s;
+wire c3;
+  
+rca_s uut (c3,s,A, B, cin) ;
+initial
+begin
+cin=0;
+A=4'b1100; B=4'b1001;
+#5 A=4'b0011; B=4'b1101;
+#5 A=4'b1110; B=4'b0001;
+
+#5 cin=1;
+A=4'b1100; B=4'b1001;
+#5 A=4'b0011; B=4'b1101;
+#5 A=4'b1110;B=4'b0001;
+#5 sfinish;
+end
+endmodule
