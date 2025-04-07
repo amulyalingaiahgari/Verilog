d771e9076e5f7d66677a96c1db44c87a7532fa48
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-07 22:52:49 +0530

    Create rca&s.v

---
+++ adders/ripple_carry_adder_subtractor/rca&s.v
@@ -0,0 +1,25 @@
+module rca_s (c3, s, A, B, cin) ;
+input [3:0]A,B;
+input cin;
+output [3:0]s;
+output c3;
+  
+wire co,cl,c2;
+  wire b0,b1,b2,b3;
+  
+assign b0=cin^B[o];
+assign bl=cin^B[1];
+assign b2=cin^B[2];
+assign b3=cin^B[3];
+FA fal (.a(A[o]), .b(be), .c(cin), .sum(s[o]), .carry(c0));
+FA fa2 (.a(A[1]), .b(b1), .c(co), .sum(s[1]), .carry(c1));
+FA fa3 (.a(A[2]), .b(b2), .c(cl), .sum(s[2]), .carry(c2));
+FA fa4 (.a(A[3]), .b(b3), .c(c2), .sum(s[3]), .carry(c3));
+endmodule
+
+module FA (sum, carry, a, b, c) ;
+input a,b,c;
+output carry,sum;
+assign sum=a^b^c;
+  assign carry=(a&b) | (c&(a^b));
+endmodule
