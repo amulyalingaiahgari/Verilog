e5f85afec42dfd0db0b60550e3eb745f2f98dab1
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-09 17:26:34 +0530

    Update 4x1_using_2x1.v

--- multiplexer/4x1mux_using_2x1mux/4x1_using_2x1.v
+++ multiplexer/4x1mux_using_2x1mux/4x1_using_2x1.v
@@ -1,21 +1,21 @@
 module mux_2x1(
-  input se0,
+  input sel0,
   input i0, i1,
   output y);
   
-  assign y = se0 ? i1 : i0;
+  assign y = sel0 ? i1 : i0;
 endmodule
 
 
 
 module mux_4x1(
-  input se0, sel,
+  input sel0, sell,
   input  i0,i1,i2,i3,
   output reg y);
   
   wire y0, y1;
   
-  mux_2_1 m1(sel, i2, i3, y1);
-  mux_2_1 m2(sel, i0, i1, y0);
-  mux_2_1 m3(se0, y0, y1, y);
+  mux_2_1 m1(sell, i2, i3, y1);
+  mux_2_1 m2(sell, i0, i1, y0);
+  mux_2_1 m3(sel0, y0, y1, y);
 endmodule
