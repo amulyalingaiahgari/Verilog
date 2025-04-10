b4e2a2f6f6db2a5ee6192878efec3ec644d77ab9
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-10 17:07:53 +0530

    Create d_asyn.v

---
+++ flip-flops/d_ff/d_asyn.v
@@ -0,0 +1,13 @@
+module d_ff (
+  input clk, rst_n,
+  input d,
+  output reg q);
+  
+  always @(posedge clk or negedge rst_n) begin
+    if(!rst_n)
+      q <= 0;
+    else     
+      q <= d;
+  end
+  
+endmodule
