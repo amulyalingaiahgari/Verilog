a6b40b8596484a75ca99ac3095e7156ecef9370a
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-10 17:26:44 +0530

    Create tb_srff.v

---
+++ flip-flops/sr_ff/tb_srff.v
@@ -0,0 +1,18 @@
+module tb;
+  reg clk, rst_n;
+  reg s, r;
+  wire q, q_bar;
+  
+  SR_flipflop ff(clk, rst, s, r, q, q_bar);
+  
+  always #2 clk = ~clk;
+  initial begin
+    clk = 0; 
+    rst = 1;
+    #4 rst =0;
+    repeat(10) @(posedge clk) begin
+      #4 s = {random}%2;
+      r = {random}%2;
+    end
+  end
+endmodule
