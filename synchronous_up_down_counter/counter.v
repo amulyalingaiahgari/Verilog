51c0ff3ed9a249203280fcf17c219cf5a75199b2
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-10 18:02:11 +0530

    Create counter.v

---
+++ synchronous_up_down_counter/counter.v
@@ -0,0 +1,17 @@
+module synchronous_counter #(parameter SIZE=4)(
+  input clk, rst_n,
+  input up/down,
+  output reg [size-1:0] count);
+
+  always @(posedge clk) begin
+    if(!rst_n) begin
+      count <= 4'h0;
+    end
+    else begin
+      if(up/down) 
+        count <= count + 1'b1;//count up
+      else 
+        count <= count - 1'b1;//count down
+    end
+  end
+endmodule
