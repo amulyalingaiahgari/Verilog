9b55bc09a6a63cc1b0e009c72ca75ea6de1fa30b
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-19 13:59:55 +0530

    Create tb_sipo.v

---
+++ shift_registers/SIPO/tb_sipo.v
@@ -0,0 +1,18 @@
+module tbsipo;
+reg clk;
+reg rst;
+reg data_in;
+wire [3:0]data_out;
+sipo uut (data_out, clk, rst,data_in);
+always #2 clk = ~clk;
+initial begin
+clk=0;
+rst=1;
+#4 rst=0;
+data_in=1;
+#5 data_in=0;
+#5 data_in=1;
+#5 data_in=0;
+#5 sfinish;
+end
+endmodule
