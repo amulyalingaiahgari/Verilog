389548f579e5f72c72de9919be8ec50e9abc831b
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-19 14:06:28 +0530

    Create tb_piso.v

---
+++ shift_registers/PISO/tb_piso.v
@@ -0,0 +1,18 @@
+module tbpiso;
+reg clk;
+reg rst;
+reg [3:0]data_in;
+reg load;
+wire data_out;
+piso uut (data_out, clk, rst, data_in, load) ;
+always #2 clk = ~clk;
+initial begin
+clk=0;
+rst=1;
+data_in=8;
+#4 rst=0;
+#4 load=1;
+#10 load=0;
+#30 $finish;
+end
+endmodule
