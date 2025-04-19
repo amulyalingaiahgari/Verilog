177b457c3135ff643473b42a647b01a4ffbb5cc3
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-19 14:10:17 +0530

    Create tb_pipo.v

---
+++ shift_registers/PIPO/tb_pipo.v
@@ -0,0 +1,18 @@
+module tbpipo ;
+reg clk;
+reg rst;
+reg [3:0]data_in;
+wire [3:0]data_out;
+pipo uut (data_out, clk, rst, data_in);
+always #2 clk = ~clk;
+initial
+begin
+clk=0;
+rst=1;
+#4 rst=0;
+data_in=5;
+#5 data_in=8;
+#5 data_in=3;
+#5 $finish;
+end
+endmodule
