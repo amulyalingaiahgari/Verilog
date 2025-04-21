82f3730ddd8ae7c1cfe6bdc35a2e2677ad881817
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-04-21 11:50:35 +0530

    Update gates.v

--- Gates/gates.v
+++ Gates/gates.v
@@ -3,15 +3,15 @@ input a, b, control;
 output y;
 
 //and g1 (y,a,b);
-or g2(y,a,b);
-not g3(y,a);
-nand g4(y,a,b);
-nor g5(y,a,b);
-xor g6(y,a,b);
-xnor g7(y,a,b);
-buf g8(y,a);
-bufifl g9(y, a, control);
-bufifo g10(y,a, control);
-notifl gll(y, a, control);
+//or g2(y,a,b);
+//not g3(y,a);
+//nand g4(y,a,b);
+//nor g5(y,a,b);
+//xor g6(y,a,b);
+//xnor g7(y,a,b);
+//buf g8(y,a);
+//bufifl g9(y, a, control);
+//bufifo g10(y,a, control);
+//notifl gll(y, a, control);
 notifo g12(y, a, control);
 endmodule
