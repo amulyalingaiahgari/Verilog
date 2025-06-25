c9479313280db0809635cc104830adb0dbb1154b
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-06-25 19:56:58 +0530

    Create tb_mealy.v

---
+++ Finite_State_Machines/mealy_sequence_detection/sequence_101_non_overlap/tb_mealy.v
@@ -0,0 +1,18 @@
+module tb_mealy;
+  reg clk;
+  reg rst;
+  reg data_in;
+  wire data_out;
+
+  non_mealy uut(clk,rst,data_in,data_out);
+  always #2 clk=~clk;
+  initial begin
+    clk=0;
+    rst=1;
+    #4 rst=0;
+    repeat(15) @(posedge clk) begin
+      #4 data_in<={$random}%2;
+    end
+    #10 $finish;
+  end
+endmodule
