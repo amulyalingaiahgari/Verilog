2f355a89c3f6a61dc20bdf581cea1cfc2a55eb7c
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-04 22:47:22 +0530

    Create tb_full_adder.v

---
+++ DAY 010 - Full Adder & Subtractor Using Half Units/full_adder/tb_full_adder.v
@@ -0,0 +1,31 @@
+module fa_tb;
+	// Inputs
+	reg a;
+	reg b;
+	reg c;
+	// Outputs
+	wire sum;
+	wire carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	fa uut (
+		.a(a), 
+		.b(b), 
+		.c(c), 
+		.sum(sum), 
+		.carry(carry)
+	);
+
+	initial begin
+		a = 0; b = 0; c = 0;
+		#10 a = 0; b = 0; c = 1;
+		#10 a = 0; b = 1; c = 0;
+		#10 a = 0; b = 1; c = 1;
+		#10 a = 1; b = 0; c = 0;
+		#10 a = 1; b = 0; c = 1;
+		#10 a = 1; b = 1; c = 0;
+		#10 a = 1; b = 1; c = 1;
+		#10 $finish;
+	end
+      
+endmodule
