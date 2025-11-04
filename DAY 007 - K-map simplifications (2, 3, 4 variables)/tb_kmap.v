8fc6a86b9f46dd7284293f528f25bcd3fa7d16a4
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-04 21:57:17 +0530

    Create tb_kmap.v

---
+++ DAY 007 - K-map simplifications (2, 3, 4 variables)/tb_kmap.v
@@ -0,0 +1,33 @@
+//======================================================
+// Day 9 - Testbench for K-map simplifications
+//======================================================
+module tb_kmap;
+
+    reg A, B, C, D;
+    wire F2, F3, F4;
+
+    kmap2 dut2 (.A(A), .B(B), .F(F2));
+    kmap3 dut3 (.A(A), .B(B), .C(C), .F(F3));
+    kmap4 dut4 (.A(A), .B(B), .C(C), .D(D), .F(F4));
+
+    initial begin
+        $display("==== 2-variable Function ====");
+        for (integer i = 0; i < 4; i++) begin
+            {A,B} = i;
+            #1 $display("A=%b B=%b -> F=%b", A, B, F2);
+        end
+
+        $display("\n==== 3-variable Function ====");
+        for (integer i = 0; i < 8; i++) begin
+            {A,B,C} = i;
+            #1 $display("A=%b B=%b C=%b -> F=%b", A, B, C, F3);
+        end
+
+        $display("\n==== 4-variable Function ====");
+        for (integer i = 0; i < 16; i++) begin
+            {A,B,C,D} = i;
+            #1 $display("A=%b B=%b C=%b D=%b -> F=%b", A, B, C, D, F4);
+        end
+        $finish;
+    end
+endmodule
