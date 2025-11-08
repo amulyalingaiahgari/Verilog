3b4e5e624945556cdc654dc0f6ec3dabf43ed36d
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-08 19:09:32 +0530

    Update and rename tb_mux.v to tb_mux.v

--- DAY 021 - Multiplexers/4x1_mux/tb_mux.v
+++ DAY 021 - Multiplexers/4x1_mux/tb_mux.v
@@ -3,7 +3,7 @@ module tb;
   reg i0,i1,i2,i3;
   wire y;
   
-  mux_example mux(sel, i0, i1, i2, i3, y);
+  mux_4x1 mux(sel, i0, i1, i2, i3, y);
   
   initial begin
     $monitor("sel = %b,i3 = %0b, i2 = %0b ,i1 = %0b, i0 = %0b,y = %0b", sel,i3,i2,i1,i0,y);
