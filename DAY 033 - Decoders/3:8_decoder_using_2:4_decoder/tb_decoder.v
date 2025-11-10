7939195632d05c80aa3432c3fc8587e4bcba400c
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-10 11:46:07 +0530

    Create tb_decoder.v

---
+++ DAY 033 - Decoders/3:8_decoder_using_2:4_decoder/tb_decoder.v
@@ -0,0 +1,29 @@
+module tb_decoder_3x8;
+    reg  [2:0] in;
+    reg        en;
+    wire [7:0] out;
+
+    // DUT instance
+    decoder_3x8_struct dut ( .in(in), .en(en), .out(out));
+
+    integer i;
+
+    initial begin
+      $display("Time | Enable | Input | Output");
+        $display("-----------------------------------");
+
+        en = 1'b0; in = 3'b000; #10;
+        $display("%0t |   %b    |  %b   | %b", $time, en, in, out);
+
+        en = 1'b1;
+        for (i = 0; i < 8; i = i + 1) begin
+            in = i; #10;
+            $display("%0t |   %b    |  %b   | %b", $time, en, in, out);
+        end
+
+        en = 1'b0; in = 3'b110; #10;
+        $display("%0t |   %b    |  %b   | %b", $time, en, in, out);
+
+        $finish;
+    end
+endmodule
