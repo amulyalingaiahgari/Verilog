8e883c8bca5ef28be71a07b68562f977a88f9299
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-10 20:45:35 +0530

    Update tb_4-bit_using_1-bit.v

--- comparator/4-bit_using_1-bit_comparator/tb_4-bit_using_1-bit.v
+++ comparator/4-bit_using_1-bit_comparator/tb_4-bit_using_1-bit.v
@@ -3,29 +3,24 @@ module four_bit_comp_tb;
 	reg [3:0] a;
 	reg [3:0] b;
 	// Outputs
-	wire lt;
-	wire eq;
-	wire gt;
+	wire lt, gt, eq;
 	
 	integer i,j;
 
 	// Instantiate the Unit Under Test (UUT)
-	four_bit_using_one_bit_comp uut (
-		.a(a), 
-		.b(b), 
-		.lt(lt), 
-		.eq(eq), 
-		.gt(gt)
-	);
+	four_bit_using_one_bit_comp uut (.a(a), .b(b), .lt(lt), .eq(eq), .gt(gt));
 
 	initial begin
-    for(i=0; i<16; i=i+1) begin
-      a=i;
-      for(j=0; j<16; j=j+1) begin
-        b=j;
-        #10;
-      end
-		end
+	  $display(" Time | A  | B  | A>B | A=B | A<B");
+      $display("-----------------------------------");
+      $monitor("  %0t | %b | %b | %b | %b | %b", $time, a, b, gt, eq, lt);
+      for(i=0; i<16; i=i+1) begin
+        a=i;
+      	for(j=0; j<16; j=j+1) begin
+          b=j;
+       	  #10;
+        end
+	  end
       $finish;
 	end
 endmodule
