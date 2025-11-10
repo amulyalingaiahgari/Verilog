3d849e237caf03c3a1562e9f9a8d83a57fc5ee87
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-10 22:00:15 +0530

    Update and rename gray_to_binary.v to gray_to_binary.v

---
+++ DAY 039 - Binary ↔ Gray Code Converter/gray_to_binary/gray_to_binary.v
@@ -0,0 +1,12 @@
+// Gray to Binary Converter
+module gray_to_binary #(parameter N = 4)(
+    input  [N-1:0] gray,
+    output reg [N-1:0] bin
+);
+    integer i;
+    always @(*) begin
+        bin[N-1] = gray[N-1]; // MSB same
+        for (i = N-2; i >= 0; i = i - 1)
+            bin[i] = bin[i+1] ^ gray[i];
+    end
+endmodule
