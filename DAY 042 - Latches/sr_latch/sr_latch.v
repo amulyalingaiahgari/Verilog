ef9916ab192e602370c1119baeff772abf3d80f1
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-11 15:00:44 +0530

    Update and rename sr_latch.v to sr_latch.v

---
+++ DAY 042 - Latches/sr_latch/sr_latch.v
@@ -0,0 +1,18 @@
+module sr_latch_en (
+  input s, r, en,
+  output reg q,
+  output qbar
+);
+  assign qbar = ~q;
+
+  always @(*) begin
+    if (en) begin
+      case ({s, r})
+        2'b00: q = q;     // Hold
+        2'b01: q = 1'b0;  // Reset
+        2'b10: q = 1'b1;  // Set
+        2'b11: q = 1'bx;  // Invalid
+      endcase
+    end
+  end
+endmodule
