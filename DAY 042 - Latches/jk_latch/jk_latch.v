94cf30cc7a89b0e06695f1fd33cfa68802e9dc65
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-11 14:30:23 +0530

    Update and rename jk_latch.v to jk_latch.v

---
+++ DAY 042 - Latches/jk_latch/jk_latch.v
@@ -0,0 +1,20 @@
+module jk_latch (
+  input j,
+  input k,
+  input en,        // enable or clock input
+  output reg q,
+  output qbar
+);
+  assign qbar = ~q;
+
+  always @(*) begin
+    if (en) begin
+      case ({j, k})
+        2'b00: q = q;      // Hold
+        2'b01: q = 1'b0;   // Reset
+        2'b10: q = 1'b1;   // Set
+        2'b11: q = ~q;     // Toggle
+      endcase
+    end
+  end
+endmodule
