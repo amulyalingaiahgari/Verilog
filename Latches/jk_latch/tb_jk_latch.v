a4f039fff21a0b52866596d9262379e2568c1baa
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-11 14:44:18 +0530

    Update tb_jk_latch.v

--- Latches/jk_latch/tb_jk_latch.v
+++ Latches/jk_latch/tb_jk_latch.v
@@ -1,16 +1,22 @@
-module tb_jk;
-  reg j;
-  reg k;
-  wire q;
-  wire qbar;
+module tb_jk_latch;
+  reg j, k, en;
+  wire q, qbar;
  
-  jk uut (j,k,q,qbar);
+  jk_latch dut (j, k, en, q, qbar);
+  
   initial begin
-    j=0;k=0;
-    #5 j=0;k=1;
-    #5 j=1;k=0;
-    #5 j=1;k=1;
-    #5 j=0;k=0;
-    #5 sfinish;
+    $display("Time | en | j | k | q | qbar");
+    $display("--------------------------");
+    $monitor("%4t | %b  | %b | %b | %b | %b", $time, en, j, k, q, qbar);
+
+    en=0; j=0; k=0;
+    #5;
+    en=1; j=0; k=0;
+    #5 j=0; k=1;
+    #5 j=1; k=0;
+    #5 j=1; k=1;
+    #5;
+    en=0; j=0; k=0;
+    #5 $finish;
   end
 endmodule
