2a9297dc8025d83fe051eedfffecf1698c5c56e2
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-12 21:27:55 +0530

    Update pipo.v

--- DAY 046 - Shift Registers/PIPO/pipo.v
+++ DAY 046 - Shift Registers/PIPO/pipo.v
@@ -1,14 +1,17 @@
 module pipo (data_out, clk, rst, data_in) ;
-input clk;
-input rst;
-input [3:0]data_in;
-output [3:0]data_out;
-reg [3:0]shift_reg;
-always @(posedge clk) begin
-if(rst)
-shift_reg <= 0;
-else
-shift_reg <= data_in;
-end
-assign data_out=shift_reg;
+  input clk;
+  input rst;
+  input [3:0]data_in;
+  output [3:0]data_out;
+  
+  reg [3:0]shift_reg;
+  
+  always @(posedge clk) begin
+    if(rst)
+      shift_reg <= 0;
+    else
+    shift_reg <= data_in;
+  end
+  
+  assign data_out=shift_reg;
 endmodule
