4565d6075d57776f13af792076bd19676e7105fb
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-12 21:30:11 +0530

    Update sipo.v

--- DAY 046 - Shift Registers/SIPO/sipo.v
+++ DAY 046 - Shift Registers/SIPO/sipo.v
@@ -1,14 +1,16 @@
 module sipo (data_out, clk, rst, data_in);
-input clk;
-input rst;
-input data_in;
-output reg [3:0]data_out;
-reg [3:0]shift_reg;
-always @(posedge clk) begin
-if(rst)
-shift_reg <= 0;
-else
-shift_reg <= {data_in, shift_reg[3:1]};
-data_out <= shift_reg;
-end
+  input clk;
+  input rst;
+  input data_in;
+  output reg [3:0]data_out;
+
+  reg [3:0]shift_reg;
+
+  always @(posedge clk) begin
+    if(rst)
+      shift_reg <= 0;
+    else
+    shift_reg <= {data_in, shift_reg[3:1]};
+    data_out <= shift_reg;
+  end
 endmodule
