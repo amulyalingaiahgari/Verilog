93c111aec0251818046cb96af5e8cef3f768aa9e
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-12 21:21:17 +0530

    Update tb_siso.v

--- shift_registers/SISO/tb_siso.v
+++ shift_registers/SISO/tb_siso.v
@@ -1,22 +1,21 @@
 module tbsiso;
-reg clk;
-reg rst;
-reg data_in;
-wire data_out;
+  reg clk;
+  reg rst;
+  reg data_in;
+  wire data_out;
   
-siso dut (data_out,clk, rst,data_in);
-always #2 clk =~ clk;
+  siso dut(data_out, clk, rst, data_in);
   
-initial
-begin
-clk=0;
-rst=1;
-#4 rst=0;
-data in=1;
-#4 data in=0;
-#4 data_in=0;
-#4 data in=1;
-#4 data in=1A
-#10 $finish;
-end
+  always #2 clk = ~clk;
+  initial begin
+    clk=0;
+    rst=1;
+    #4 rst=0;
+    data_in=1;
+    #4 data_in=0;
+    #4 data_in=0;
+    #4 data_in=1;
+    #4 data_in=1;
+    #10 $finish;
+  end
 endmodule
