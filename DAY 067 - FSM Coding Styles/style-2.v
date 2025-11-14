50c3ec43dd0a8c61158800f1e6b0c465b45f69a8
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-14 20:57:05 +0530

    Update and rename Finite_State_Machines/fsm_styles/fsm2.v to DAY 067 - FSM Coding Styles/style-2.v

--- DAY 067 - FSM Coding Styles/style-2.v
+++ DAY 067 - FSM Coding Styles/style-2.v
@@ -1,4 +1,8 @@
-//fsm coding style 2
+//Style–2 → “Two-Process FSM / 2-Always Block FSM”
+//-One block: State register
+//-One block: Next-state logic
+//-Outputs generated using continuous assignments
+// 2-Process Moore FSM (Next-state logic + Registered state + separate output decode)
 module fsm (
   input clk,
   input rst,
@@ -6,27 +10,29 @@ module fsm (
   input ws,
   output reg rd,
   output reg ds );
+
   reg [1:0]state, next_state;
+  
   parameter IDLE=2'b00,
             s1=2'b01,
             s2=2'b10,
             s3=2'b11;
-//present state sequential block
+  
+  // State register
   always @(posedge clk) begin
     if(rst)
       state <= IDLE;
     else
       state <= next_state;
   end
-//next state combinational always block
-  always @(state, g0, ws) begin
+  
+  //Next state logic
+  always @(*) begin
     next_state <= IDLE;
     case (state)
       IDLE:begin
         if(g0)
           next_state <= s1;
-        else
-          next_state <= IDLE;
       end
       sl:begin
         next_state <= s2;
@@ -42,7 +48,10 @@ module fsm (
       end
     endcase
   end
-  //continuous assignment outputs
+  
+  //continuous assignment outputs 
+  //Output decoding (Moore)
   assign rd=(state==s1)||(state==s2);
   assign ds=(state==s3);
+  
 endmodule
