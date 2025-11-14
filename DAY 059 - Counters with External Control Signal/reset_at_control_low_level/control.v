1c21264e60ca8a435ba5017247159ef67a79b44f
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-14 18:07:15 +0530

    Create control.v

---
+++ DAY 059 - Counters with External Control Signal/reset_at_control_low_level/control.v
@@ -0,0 +1,19 @@
+module dc (count, clk, rst, control) ;
+  input control;
+  input clk;
+  input rst;
+  output reg [3:0]count;
+  
+  always @(posedge clk) begin
+    if (rst)
+      count <= 0;
+    else
+    if (control)
+      count <= (count+1) ;
+    else
+    count <= 0;
+  end
+endmodule
+
+//when control is high --- increment
+//when control is low --. reset
