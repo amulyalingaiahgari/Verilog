a3da6434b05006f4f4ff4e37f38b0b22183002f0
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-14 20:21:21 +0530

    Update and rename Finite_State_Machines/fsm_styles/tbfsm.v to DAY 067 - FSM Coding Styles/tb_fsm.v

--- DAY 067 - FSM Coding Styles/tb_fsm.v
+++ DAY 067 - FSM Coding Styles/tb_fsm.v
@@ -7,12 +7,16 @@ module tbfsm;
   wire ds;
 
   fsm uut (clk, rst, g0, ws, rd, ds);
+  
   always #2 clk= ~clk;
   initial begin
+    $monitor("T=%0t | g0=%b ws=%b | rd=%b ds=%b | state change", $time, g0, ws, rd, ds);
     clk=0;
     rst=1;
-    #4 rst=0;
     g0=0;
+    ws=0;
+    
+    #4 rst=0;
     #4 g0=1;
     #8 ws=1;
     #4 ws=0;
