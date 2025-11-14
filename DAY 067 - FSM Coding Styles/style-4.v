301d6eafa366f3b1b35b6314afde53e5f5534f09
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-14 21:12:35 +0530

    Update and rename Finite_State_Machines/fsm_styles/fsm4.v to DAY 067 - FSM Coding Styles/style-4.v

--- DAY 067 - FSM Coding Styles/style-4.v
+++ DAY 067 - FSM Coding Styles/style-4.v
@@ -1,4 +1,9 @@
-//fsm coding style 4
+//Style–4 → “Three-Process FSM / 3-Always Block FSM”
+//-One block: State register
+//-One block: Next-state logi
+//-One block: Output register logic (registered outputs)
+////3-Process Moore FSM (Registered Outputs)////
+
 module fsm (
   input clk,
   input rst
@@ -6,43 +11,46 @@ module fsm (
   input ws,
   output reg rd,
   output reg ds );
+  
   reg [1:0]state, next_state;
+  
   parameter IDLE=2'b00,
             s1=2'b01,
             s2=2'b10,
             s3=2'b11;
-//present state sequential block
+  
+  //state register
   always @(posedge clk) begin
     if(rst)
       state <= IDLE;
     else
       state <= next_state;
   end
-//next state ombinational always block
+  
+  //next state logic
   always @(state, g0, ws) begin
-    next_state <= IDLE;
+    next_state = IDLE;
     case (state)
       IDLE:begin
         if(g0)
-          next_state <= s1;
-        else
-          next_state <= IDLE;
+          next_state = s1;
       end
       sl:begin
-        next_state <= s2;
+        next_state = s2;
       end
       s2:begin
         if (ws)
-          next_state <= s1;
+          next_state = s1;
         else
-          next_state <= s3;
+          next_state = s3;
       end
       s3:begin
-        next_state <= IDLE;
+        next_state = IDLE;
       end
     endcase
   end
-//registered outputs sequential always block
+  
+  //registered output logic
   always @(posedge clk or posedge rst) begin
     if(rst) begin
       rd <= 0;
@@ -52,9 +60,10 @@ module fsm (
       rd <= 0;
       ds <= 0;
       case (state)
-        IDLE: {rd, ds} <= {0, 0};
-        s1,s2: rd <= 1;
-        s3 <= ds <= 1;
+        IDLE: begin rd <= 0; ds <= 0; end
+        s1:   begin rd <= 1; ds <= 0; end
+        s2:   begin rd <= 1; ds <= 0; end
+        s3:   begin rd <= 0; ds <= 1; end
       endcase
     end
   end
