b6392c86ad89fddcf489cca708c258490a34e646
Amulya Lingaiahgari <151658673+amulyalingaiahgari@users.noreply.github.com>
2025-11-14 20:47:10 +0530

    Update and rename Finite_State_Machines/fsm_styles/fsm1.v to DAY 067 - FSM Coding Styles/style1.v

--- DAY 067 - FSM Coding Styles/style1.v
+++ DAY 067 - FSM Coding Styles/style1.v
@@ -1,46 +1,52 @@
-module fsm
+// Style–1 → “Two-Process FSM / 2-Always Block FSM”
+// - Next-state logic + Output logic inside one combinational block
+// - State update inside another sequential block
+
+module fsm (
   input clk,
   input rst,
   input g0,
   input ws,
   output reg rd,
   output reg ds );
+  
   reg [1:0]state, next_state;
+  
   parameter IDLE=2'b00,
             s1=2'b01,
             s2=2'b10,
             s3=2'b11;
-  
+
+  // State register
   always @(posedge clk) begin
     if(rst)
       state <= IDLE;
     else
       state <= next_state;
   end
-  
-  always @(state,g0,ws) begin
+
+  // Next-State + Output logic
+  always @(*) begin
     case (state)
       IDLE:begin
-        rd <= 0; ds <= 0;
+        rd = 0; ds = 0;
         if(g0)
-          next_state <= s1;
-        else
-        next_state <= IDLE;
+          next_state = s1;
       end
       sl:begin
-        rd <= 1; ds=0;
-        next_state <= s2;
+        rd = 1; ds=0;
+        next_state = s2;
       end
       s2:begin
-        rd <= 1; ds=0;
+        rd = 1; ds=0;
         if (ws)
-          next_state <= s1;
+          next_state = s1;
         else
-        next_state <= s3;
+          next_state = s3;
       end
       s3:begin
-        rd<=0; ds<=1;
-        next_state<=IDLE;
+        rd=0; ds=1;
+        next_state = IDLE;
       end
     endcase
   end
